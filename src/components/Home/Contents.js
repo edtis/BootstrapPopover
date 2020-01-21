8f9095f53b490f98587dc59ffd9939cd85a7dabe
ranjeet <ranjeet.kumar@tothenew.com>
2020-01-21 11:55:16 +0530

    added popover functionality

--- src/components/Home/Contents.js
+++ src/components/Home/Contents.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Component } from "react";
+import Popover from "react-text-selection-popover";
 import get from "lodash/get";
-import $ from "jquery";
 
-import { Row, Popover, OverlayTrigger, Col  } from 'react-bootstrap';
+import { Row, Col  } from 'react-bootstrap';
 
-class Contents extends React.Component {
+class Contents extends Component {
+  state = {
+    startCoords: 0,
+    endCoords: 0,
+  };
+
   constructor(props) {
     super(props);
-    this.state = {
-      startCoords: 0,
-      endCoords: 0,
-      selection: ''
-    }
+    this.refParagraph = React.createRef();
+    this.refCode = React.createRef();
   }
 
   handleMouseDown = (event) => {
@@ -27,62 +29,28 @@ class Contents extends React.Component {
     let x = event.clientX;
     let y = event.clientY;
     let endCoords = "End position X: " + x + ", End position Y: " + y;
-    let selection = this.getSelected();
-        selection = $.trim(selection);
-        this.setState({
-          endCoords: endCoords,
-          selection: selection
-        })
-  }
-
-  getSelected = () => {
-    if (window.getSelection) {
-      return window.getSelection();
-    } else if (document.getSelection) {
-      return document.getSelection();
-    } else {
-      var selection = document.selection && document.selection.createRange();
-      if (selection.text) {
-        return selection.text;
-      }
-      return false;
-    }
+    this.setState({
+      endCoords: endCoords,
+    })
   }
 
   render() {
     const { contents } = this.props;
-    const { startCoords, endCoords, selection } = this.state;
-    let contentLength = selection.length;
+    const { startCoords, endCoords } = this.state;
     return (
       <React.Fragment>
-        {
-         get(contents, "chapters", []).map((chapter, index) => {
-            return (
-              <React.Fragment key={`${index}-${index}`}>
-                <div className="chapters">Chapter {get(chapter, 'chapter', null)}</div>
-                {
-                  get(chapter, 'verses', []).map((verse, index) => {
-                    return (
-                      <React.Fragment key={index}>
-                        <OverlayTrigger
-                          placement="bottom"
-                          trigger="click"
-                          rootClose={true}
-                          overlay={
-                            <Popover 
-                              id={contentLength > 0 ? 'popover-basic' : 'popover-basic-hide'}
-                            >
-                              {
-                                contentLength > 0 && (
-                                  <Popover.Content>
-                                    <p>{startCoords}</p>
-                                    <p>{endCoords}</p>
-                                  </Popover.Content>
-                                )
-                              }
-                            </Popover>
-                          }
-                        >
+        <div
+          ref={this.refParagraph}
+        >
+          {
+            get(contents, "chapters", []).map((chapter, index) => {
+              return (
+                <React.Fragment key={`${index}-${index}`}>
+                  <div className="chapters">Chapter {get(chapter, 'chapter', null)}</div>
+                  {
+                    get(chapter, 'verses', []).map((verse, index) => {
+                      return (
+                        <React.Fragment key={index}>
                           <Row 
                             onMouseDown={event => this.handleMouseDown(event)}
                             onMouseUp={event => this.handleMouseUp(event)}
@@ -91,17 +59,23 @@ class Contents extends React.Component {
                             <Col sm={1} className="verse-number">{verse.verse}.</Col>
                             <Col sm={11} className="verse-text">{verse.text}</Col>
                           </Row>
-                        </OverlayTrigger>
-                      </React.Fragment>
-                    )
-                  })
-                }
-              </React.Fragment>
-            )
-          })
-        }
+                        </React.Fragment>
+                      )
+                    })
+                  }
+                </React.Fragment>
+              )
+            })
+          }
+        </div>
+        <Popover selectionRef={this.refParagraph}>
+          <div className="popover-content">
+            <p>{startCoords}</p>
+            <p>{endCoords}</p>
+          </div>
+        </Popover>
       </React.Fragment>
-    )
+    );
   }
 }
 
